f8705b04a7d93925d25fa0a1c5092dece6ff5923
unknown <psahmed96@gmail.com>
2021-10-17 23:27:09 +0300

    Testing

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ const _rollbarConfig = {
   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
+  handleUncaughtException: true,
+  enabled: true,
+  source_map_enabled: true,
+  addErrorContext: true,
+  autoInstrument: true,
+  contentSecurityPolicy: true,
+  errorOnContentSecurityPolicy: true,
+  server: {
+    host: 'localhost',
+    root: 'C:\\Users\\Ahmed\\Desktop\\testing\\',
+    branch: 'master'
+  },
   payload: {
     environment: process.env.REACT_APP_ENVIROMENT,
     client: {
@@ -17,10 +29,6 @@ const _rollbarConfig = {
         code_version: process.env.REACT_APP_GIT_SHA,
         guess_uncaught_frames: true
       }
-    },
-    server: {
-      root: 'http://localhost/',
-      branch: 'gh-pages'
     },
     person: {
       id: Math.random().toString(), // required
@@ -53,6 +61,7 @@ function App() {
     console.log(process.env.REACT_APP_GIT_SHA);
     console.log(process.env.REACT_APP_ENVIROMENT);
   }, []);
+  // const rollbar = useRollbar();
   // useRollbar(rollbar);
   return (
     <Provider config={_rollbarConfig}>
@@ -72,11 +81,9 @@ function App() {
                     'This is Testing Error For Last Time Clicking'
                   );
                 } catch (e) {
+                  // rollbar.error('data provider   error', e);
                   Rollbar.error(e);
-                  console.log(
-                    '🚀 ~ file: App.js ~ line 63 ~ App ~ Error',
-                    e.message
-                  );
+                  console.log('🚀 ~ file: App.js ~ line 63 ~ App ~ Error', e);
                 }
               }}
             >
